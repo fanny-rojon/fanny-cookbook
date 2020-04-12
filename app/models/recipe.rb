b196f59bd5711177f8dbd3497ae0b2fbced1ffa8
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-12 19:30:04 +0200

    added relationships & validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :category
   belongs_to :ingredient
+
+  validates :name, :prep_time, :ingredients_list, :preparation, :category_id, :ingredient_id, presence: true
 end
